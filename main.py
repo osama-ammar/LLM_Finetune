b18d390d0081d854eba6131bc2a01f539f9eaf9e
Osama Ammar <54119005+osama-ammar@users.noreply.github.com>
2024-12-17 21:19:12 +0200

    add quanization

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, Dataset
 from transformers import GPT2Tokenizer, GPT2LMHeadModel , pipeline
-from peft import get_peft_model, LoraConfig, TaskType
+from peft import get_peft_model, LoraConfig, TaskType   # Supports QLoRA and LoRA.
 import mlflow
 import os
 from helper_functions import *
 import yaml
 from sklearn.model_selection import train_test_split
 import warnings
-# from optimum.onnxruntime import ORTModelForCausalLM
-# from optimum.onnxruntime.configuration import OptimizationConfig
+import bitsandbytes as bnb  # For quantization
 
 
 warnings.filterwarnings("ignore")
@@ -21,15 +20,16 @@ with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
     
 # Dynamically set the device parameter
-device = config["training"]["device"]
 use_mlflow = config["training"]["use_mlflow"]
 epochs = config["training"]["epochs"]
 batch_size=config["training"]["batch_size"]
 output_path = config["training"]["output_dir"]
+quatize = config["training"]["quatize"]
 
 lora_rank = config["lora"]["rank"]
 lora_alpha = config["lora"]["alpha"]
 lora_dropout = config["lora"]["dropout"]
+
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 lr = config["optimizer"]["lr"]
@@ -53,16 +53,7 @@ class TextDataset(Dataset):
             "input_ids": self.encodings.input_ids[idx].to(device),
             "attention_mask": self.encodings.attention_mask[idx].to(device) # The attention mask is a tensor that tells the model which parts of the input sequence are real data (tokens of the original text) and which parts are just padding.
         }
-
-# Step 3: Fine-tuning with LoRA
-lora_config = LoraConfig(
-    task_type=TaskType.CAUSAL_LM,   # Define the task type (causal language model) This type of model predicts the next word/token in a sequence based on the input context. Common examples include GPT-style models like GPT-2, GPT-3, etc. Why Specify: Different tasks (e.g., causal LM, sequence classification, masked LM) have different objectives,
-    inference_mode=False,           # Set to False for fine-tuning
-    r=lora_rank,                           # Rank of the LoRA matrices (low-rank factorization) lower rank ---> smaller metrices to fine tune ---> less GPU consumption and faster training
-    lora_alpha=lora_alpha,                  # Scaling factor for LoRA :The LoRA updates are scaled by dividing them by `lora_alpha`  ,  The updates become smaller relative to the pre-trained weights.This leads to conservative fine-tuning, meaning the model changes less and retains more of its pre-trained behavior , Preserving the original knowledge of the pre-trained model (higher lora_alpha). , Adapting the model to new data (lower lora_alpha)..
-    lora_dropout=lora_dropout                # Dropout rate to avoid overfitting
-)
-
+    
 # we will fine tune the model over this text
 texts = [
             "osama is working at atomica",
@@ -73,11 +64,8 @@ texts = [
             "osama holds a MSC degree."
         ]
 
-#moving model ind data to cuda
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 tokenizer.pad_token = tokenizer.eos_token  # Ensure padding compatibility
-
 
 # Split dataset into training and validation sets
 train_texts, val_texts = train_test_split(texts, test_size=0.3, random_state=42)
@@ -90,8 +78,22 @@ train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
 
+# Fine-tuning with LoRA
+lora_config = LoraConfig(
+    task_type=TaskType.CAUSAL_LM,   # Define the task type (causal language model) This type of model predicts the next word/token in a sequence based on the input context. Common examples include GPT-style models like GPT-2, GPT-3, etc. Why Specify: Different tasks (e.g., causal LM, sequence classification, masked LM) have different objectives,
+    inference_mode=False,           # Set to False for fine-tuning
+    r=lora_rank,                           # Rank of the LoRA matrices (low-rank factorization) lower rank ---> smaller metrices to fine tune ---> less GPU consumption and faster training
+    lora_alpha=lora_alpha,                  # Scaling factor for LoRA :The LoRA updates are scaled by dividing them by `lora_alpha`  ,  The updates become smaller relative to the pre-trained weights.This leads to conservative fine-tuning, meaning the model changes less and retains more of its pre-trained behavior , Preserving the original knowledge of the pre-trained model (higher lora_alpha). , Adapting the model to new data (lower lora_alpha)..
+    lora_dropout=lora_dropout                # Dropout rate to avoid overfitting
+)
+
+
+
 # Step 2: Model Initialization
-model = GPT2LMHeadModel.from_pretrained("gpt2")
+model = GPT2LMHeadModel.from_pretrained("gpt2",
+                                        load_in_4bit=True if device=="cuda" and quatize  else False,  # Enable 4-bit quantization
+                                        )
+
 model.resize_token_embeddings(len(tokenizer))
 model = get_peft_model(model, lora_config)
 model.to(device)
@@ -120,7 +122,6 @@ def validation_epoch():
         
     return validation_loss
     
-    
 def run_training(model_path):
     for epoch in range(epochs):
         training_loss = training_epoch()
@@ -139,7 +140,8 @@ def run_training(model_path):
     model.save_pretrained(output_path)
     tokenizer.save_pretrained(output_path)
     #onnx_export(output_path)
-###############################################################################################3
+    
+
 def mlflow_log_model(model):
     
         mlflow.log_param("learning_rate", lr)
@@ -182,8 +184,6 @@ def mlflow_log_model(model):
             model_config=model_config,
             )
         return model_info
-##################################################################################################
-
 
 if mode=="train":
     model_path= f"{output_path}/gpt2_model.pth"
@@ -202,12 +202,3 @@ if mode=="train":
     else:
         run_training()
 
-#############################################################################
-
-
-
-
-
-# serve th#e mode l: mlflow models serve --model-uri "runs:/<run_id>/model" --host 0.0.0.0 --port 5001
-# . Use the Model for Inference
-# Set up a Web Interface for the Conversational Model
